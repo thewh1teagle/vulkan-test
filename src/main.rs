b181d93fb6b2f7e6e7ce072b80e0a293500a3a4c
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-09-24 00:26:14 +0300

    feat: show device info

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rfd::{MessageButtons, MessageDialog, MessageLevel};
+use std::io::Read;
 
 fn init_vulkan() -> Result<(), Box<dyn std::error::Error>> {
     use ash::vk;
@@ -7,6 +7,16 @@ fn init_vulkan() -> Result<(), Box<dyn std::error::Error>> {
         let app_desc = vk::ApplicationInfo::default().api_version(vk::make_api_version(0, 1, 0, 0));
         let instance_desc = vk::InstanceCreateInfo::default().application_info(&app_desc);
         let instance = entry.create_instance(&instance_desc, None)?;
+        for (index, device) in instance.enumerate_physical_devices()?.iter().enumerate() {
+            let properties = instance.get_physical_device_properties(*device);
+            let name = properties.device_name;
+            let name = String::from_utf8(name.map(|i| i as u8).to_vec())?;
+            let device_type = properties.device_type;
+            println!(
+                "Device: {}\nName: {}\nType: {:?}\n",
+                index, name, device_type
+            );
+        }
         instance.destroy_instance(None);
     }
     Ok(())
@@ -15,20 +25,11 @@ fn init_vulkan() -> Result<(), Box<dyn std::error::Error>> {
 fn main() {
     match init_vulkan() {
         Ok(_) => {
-            MessageDialog::new()
-                .set_level(MessageLevel::Info)
-                .set_buttons(MessageButtons::Ok)
-                .set_title("Success")
-                .set_description("Load Vulkan Successfully")
-                .show();
+            println!("✅ Load Vulkan Successfully");
         }
         Err(error) => {
-            MessageDialog::new()
-                .set_level(MessageLevel::Error)
-                .set_buttons(MessageButtons::Ok)
-                .set_title("Error")
-                .set_description(format!("Load Vulkan failed: {:?}", error))
-                .show();
+            println!("❌ Load Vulkan failed: {:?}", error);
         }
     }
+    let _ = std::io::stdin().read(&mut [0u8]).unwrap();
 }
